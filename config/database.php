f0701ad2fd84b21e106c34eeff8a2acfd5d8c0bf
JMdjn <157415391+JMdjn@users.noreply.github.com>
2024-01-22 22:19:14 +0100

    fix password

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', '3306');
 define('DB_NAME', 'recipe_app');
 define('DB_USER', 'root');
-define('DB_PASS', 'caca');
+define('DB_PASS', '');
 
 try {
     $pdo = new PDO(
